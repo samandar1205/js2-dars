d599d2e3bc3cddee5d56977ef54a1efe5b1fc063
samandar12 <samandarkomiljonov1212@gmail.com>
2023-05-17 21:43:27 +0500

    Bu js1-dars kodlari yuklandi

---
+++ JS/main.js
@@ -0,0 +1,59 @@
+/*
+if(agar)
+else(u holda)
+else if (yoki bo'lmasa)
+*/
+
+/*if('salom'=='salom'){
+    document.write("Ular teng!");
+}
+else{
+    document.write('Ular teng emas!')
+}
+if (112==112){
+    document.write("Ular teng!");
+}
+else{
+    document.write("Ular teng emas!")
+}*/
+/*if(1000==1000){
+    document.write("1-Tog'ri")
+}*/
+else if('salom'=='salo'){
+    document.write("2-Tog'ri")
+}
+/*else{
+    document.write("Yuqoridagilarning hech biri teng emas!")
+}*/
+/*let a="Familiya";
+let b="Ism";
+let c="Email";
+let d="Parol";*/
+/*if (a=="Familiya" && b=="Ism" && c=="Email" && d=="Parol"){
+    document.write("Tastiqlandi!")
+}
+else{
+    document.write("Tasdiqlanmadi    |'_'|    !")
+}*/
+/* bu ^ &&-kodda bitasi notogri bosa notogri chiqaradi.*/
+
+/*if( a==10 || b==19 || c==46 || d==49){
+    document.write("Shularning hamasi bajarildi!")
+}
+else{
+    document.write("bajarilmadi!")
+}*/
+/*bu ^ ||-kodda bittasi xato bolsa ham togri chiqaradi.*/
+
+/*JS DATA TYPES:UNDEFINED,BOOLEAN,OBJEKT*/
+
+/*let son
+document.write(typeof son)
+*/
+/*let rost=false
+document.write(rost)*/
+
+
+
+
+
